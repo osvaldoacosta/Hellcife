3095321a99f233428e888f7ca1c5aa8c71d3b709
osvaldoacosta <osvaldocosta.smhc@gmail.com>
2023-02-09 08:13:38 -0300

    CU-864dxt271 - Desenvolver sistema de mira ao segurar botao direito

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    private Camera mainCamera;
+    [SerializeField] PlayerMovement pm;
+
+    void Start()
+    {
+        this.mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseMovement();
+    }
+    private void MouseMovement()
+    {
+      if(Input.GetMouseButton(1)){
+        pm.setMovSpeed(3.5f);
+        Vector3 pointToLook = GetPointToLook();
+        transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+      }else if(Input.GetMouseButtonUp(1)){
+        pm.setMovSpeed(7f); 
+      }
+
+    }
+
+    public Vector3 GetPointToLook()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+        if (groundPlane.Raycast(ray, out rayLength))
+        {
+            return ray.GetPoint(rayLength);
+        }
+       return Vector3.zero;
+    }
+}
