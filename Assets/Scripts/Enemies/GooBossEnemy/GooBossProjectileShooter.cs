a2410ad1253fb234fe3bc703bd87da53d1ca86fe
brederodes <anacleciobrederodes@outlook.com>
2023-02-27 11:34:46 -0300

    #864e1pd6w - adicionando chefe gosmento

---
+++ Assets/Scripts/Enemies/GooBossEnemy/GooBossProjectileShooter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooBossProjectileShooter : MonoBehaviour
+{
+    public GameObject gooProjectileObjectPool;
+    void Start()
+    {
+        return;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        return;
+    }
+    public void launchGooProjectile(Vector3 launchPoint, 
+    float parabolaMaxHeight,
+    Vector3 gooProjectileLandingCoord,
+    float gooProjectileAirTime,
+    float gooPuddleRadius,
+    float gooPuddleDuration){
+        //get projectile from object pool
+        GameObject newGooProjectile = gooProjectileObjectPool.GetComponent<ObjectPool>().GetPooledObject(); 
+        if (newGooProjectile != null) {
+            newGooProjectile.transform.position = transform.position;
+            newGooProjectile.transform.rotation = transform.rotation;
+        }
+        GooProjectileBehaviour gooProjectileBehaviour = newGooProjectile.GetComponent<GooProjectileBehaviour>();
+        gooProjectileBehaviour.setGooShot(launchPoint ,parabolaMaxHeight, gooProjectileLandingCoord, gooProjectileAirTime, gooPuddleRadius, gooPuddleDuration);
+    }
+}
