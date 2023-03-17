2a3b13012f0a3693400cf19a3cd05df97e8b3d27
osvaldoacosta <osvaldocosta.smhc@gmail.com>
2023-03-17 00:48:46 -0300

    adicionando tela de upgrade do player

---
+++ Assets/Scripts/Shop/GunShop.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShop : MonoBehaviour
+{
+    public List<GunInfo> guns; //Sera usado no hud
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Shop/ProximityDetector.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ProximityDetector : MonoBehaviour
+{
+    public GameObject objectToDetect;
+    public GameObject loja;
+    public GameObject Texto;
+    public float proximityThreshold = 2f;
+
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, objectToDetect.transform.position);
+        if (distance < proximityThreshold)
+        {
+            Texto.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                loja.SetActive(true);
+            }
+        }
+        else
+        {
+            Texto.SetActive(false);
+            loja.SetActive(false);
+        }
+    }
+}
