ad3cdef8ad36a57f957f859cb9271cbb7162c573
Osvaldo Costa <osvaldocosta.smhc@gmail.com>
2023-03-18 00:55:46 -0300

    Adicionando a mecanica antiga da loja na ui feita

---
+++ Assets/Shop/Shop.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    private PlayerPoints playerPoints;
+
+    private void Awake()
+    {
+        playerPoints = GetComponent<PlayerPoints>();
+    }
+
+    public bool CanPlayerBuy(uint price) => playerPoints.GetPoints() >= price;
+
+   
+}
