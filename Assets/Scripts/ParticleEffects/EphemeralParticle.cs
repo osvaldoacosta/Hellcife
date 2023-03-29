9f28708c51149adf56f19db1faf58459644827dc
brederodes <anacleciobrederodes@outlook.com>
2023-03-28 21:22:06 -0300

    adicionando particulas do player e dos inimigos

---
+++ Assets/Scripts/ParticleEffects/EphemeralParticle.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EphemeralParticle : MonoBehaviour
+{
+    [SerializeField] public float duration;
+    float timeToStopEmmiting= -1f;
+    bool isEmmiting = false;
+    
+    
+    public void Emit(int numberOfParticles){
+        ParticleSystem ps = gameObject.GetComponent<ParticleSystem>();
+        if(ps==null) return;
+        gameObject.SetActive(true);
+        isEmmiting = true;
+        timeToStopEmmiting= Time.time + duration;
+        ps.Emit(numberOfParticles);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(isEmmiting){
+            if(Time.time >= timeToStopEmmiting){
+                gameObject.SetActive(false);
+                isEmmiting= false;
+            }
+        }
+    }
+}
